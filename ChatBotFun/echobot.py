952ed68e8d53f47a209ff28fc7cea4824c4e20d2
Jillian Hade <jhade@shiftgig.com>
2017-11-05 12:52:03 -0600

    adds main function

--- ChatBotFun/echobot.py
+++ ChatBotFun/echobot.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import time
 
 TOKEN = '492715883:AAFlCHFSJY6KKvwPVlAQZ-e5bIbCmV1eGxI'
 URL = 'https://api.telegram.org/bot{}/'.format(TOKEN)
@@ -36,5 +37,15 @@ def send_message(text, chat_id):
     get_url(url)
 
 
-text, chat = get_last_chat_id_and_text(get_updates())
-send_message(text, chat)
+def main():
+    last_textchat = (None, None)
+    while True:
+        text, chat = get_last_chat_id_and_text(get_updates())
+        if (text, chat) != last_textchat:
+            send_message(text, chat)
+            last_textchat = (text, chat)
+        time.sleep(0.5)
+
+
+if __name__ == '__main__':
+    main()
